1c7b6b6636784237e7970290c3e631aa18b15a4b
daniel mamani <dani.oscar.257@gmail.com>
2020-05-15 14:46:22 -0300

    OK: primer commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//S: UI section
 const inputs = document.querySelectorAll(".input");
 
 
@@ -18,3 +20,51 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+////////////////////////////////////////////
+
+//S: login form
+
+let ServerLogin= function(username,password){  //A: manda un POST al endpoint con el usuario y contraseña devuelve una promesa
+  return new Promise(function(resolve, reject){
+    var raw = {
+      username: username,
+      password: password
+    }    
+    var requestOptions = {
+      method: 'POST',
+      body:  JSON.stringify(raw),
+      redirect: 'follow',
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    };
+    
+    fetch("https://dabau-api.herokuapp.com/api/login", requestOptions)
+      .then(response => response.text())
+      .then(result => resolve(result))
+      .catch(error => reject(error));
+
+  })
+}
+
+
+window.onload=function() {
+  document.getElementById('loginForm').onsubmit=function(e) {
+    e.preventDefault();
+
+    let username= document.getElementById("username").value;  
+    let password= document.getElementById("password").value;  
+    if(!username || !password) {return alert("usuario y contraseña obligatorios");}
+    console.log("username: " ,username, " password: " , password)
+    ServerLogin(username, password)
+      .then( result=>{
+        let resultObj= JSON.parse(result);
+        if (resultObj.status=="error" ) return alert(resultObj.msg)
+        localStorage.setItem('token', resultObj.token);
+        alert("Login correcto token guardado en localstorage")
+      })
+      .catch( err=>{
+        console.log("Login error: " , err)
+      } )
+  }
+}
